d820b9d1ec669a747a66800f70a7e72e16af3e54
TakedaGoro <goro.takeda@divx.co.jp>
2023-09-25 17:20:18 +0900

    admin側のログイン、ログアウト、ユーザー登録

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-	post 'api/check_user', to: 'user_auth#check_user'
-	post 'api/register', to: 'users#create'
+  namespace :admin do
+    root 'home#index'
+  end
+  devise_for :admins, controllers: { sessions: 'admins/sessions' }
+  post 'api/check_user', to: 'user_auth#check_user'
+  post 'api/register', to: 'users#create'
 end
+
