66c50505608623031406772f63d4f3f4d06047d9
TakedaGoro <goro.takeda@divx.co.jp>
2023-09-28 18:34:58 +0900

    バックエンド側のカレンダー表示を修正

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -12,6 +12,8 @@ module Myapp
     config.load_defaults 7.0
     config.hosts.clear
     config.time_zone = 'Tokyo'
+    config.beginning_of_week = :sunday
+    config.i18n.default_locale = :ja
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
